467851327e0d4663cc633b400665da893b7b72c7
Ulf Hamster <554c46@gmail.com>
2020-10-08 15:31:59 +0200

    web app activated

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # start all containers
-folders=(database restapi)
+folders=(database restapi webapp)
 for folder in ${folders[@]}; do
     nohup docker-compose -f "./${folder}/docker-compose.yml"  up --build >/dev/null 2>&1 &
 done
